d9a538182e1804b85c3e57e078b664d34d11aee9
Oleg Maksimovich <omaksimovich@nautilus.com>
2023-12-25 11:56:36 +0100

    update code

--- src/main/kotlin/com/example/Handler.kt
+++ src/main/kotlin/com/example/Handler.kt
@@ -1,9 +1,24 @@
 package com.example
 
-fun main(args: Array<String>) {
-    println("Hello World!")
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.RequestHandler
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
+import com.google.gson.GsonBuilder
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+class Handler : RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    private val mapper = GsonBuilder().setPrettyPrinting().create()
+    override fun handleRequest(input: APIGatewayProxyRequestEvent, context: Context): APIGatewayProxyResponseEvent {
+        val logger = context.logger
+
+        // log execution details
+        logger.log("ENVIRONMENT VARIABLES: " + mapper.toJson(System.getenv()))
+        logger.log("CONTEXT: " + mapper.toJson(context))
+        // process event
+        logger.log("EVENT: " + mapper.toJson(input))
+
+        return APIGatewayProxyResponseEvent()
+            .withStatusCode(200)
+            .withBody("Hello from Kotlin Lambda!")
+    }
 }
